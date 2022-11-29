12e53c0fca88544254972a6eedacd379d1228f6d
Olivia Beck <olb5053@psu.edu>
2022-11-29 14:41:24 -0500

    class notes updates

--- class-notes/week-13/in-class-example-regression.R
+++ class-notes/week-13/in-class-example-regression.R
@@ -1,6 +1,8 @@
 
 ## Investigate data --------------------------------
+?mtcars
 head(mtcars)
+str(mtcars)
 
 
 
@@ -11,11 +13,35 @@ mtcars <- mtcars %>%
 str(mtcars)
 
 #model
-mod1 <- lm( RESPONSE.VARIABLE ~  PREDICTOR1 + PREDICTOR2 +    ,data = Carseats)
-
-
+mod1 <- lm( qsec ~  vs + wt + hp +cyl, data = mtcars)
 
 ## Inspect results ------------------------------------------
 summary(mod1)
 mod1$coefficients
+mod1$residuals
+mod1$fitted.values
+
+
+
+#### In class model 
+
+# response  = weight 
+# predictors = engine, transmission, cylinders, carborators 
+
+
+mod2 <- lm(wt ~ vs + am + cyl + carb, data = mtcars)
+summary(mod2)
+
+
+#### All variables 
+
+
+mod3 <- lm( wt ~ ., data = mtcars)
+summary(mod3)
+
+
+#### Regression Trees 
+
+mod1.tree <- rpart( qsec ~  vs + wt + hp +cyl, data = mtcars)
+prp(mod1.tree)
 
